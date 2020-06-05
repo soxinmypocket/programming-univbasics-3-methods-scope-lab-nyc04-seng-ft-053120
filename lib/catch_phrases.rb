d04fbf7d3ffb870c9ffdc6cbe7c9ab19adb45aff
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-05 17:40:16 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,3 +8,13 @@ end
 def toadstool
   puts $status
 end
+
+def all_phrases
+  describe "all_phrases" do
+  it "puts out all of the previous catch phrases" do
+    expect{all_phrases}.to output(/It's-a me, Mario!\n/).to_stdout
+    expect{all_phrases}.to output(/Thank You Mario! But Our Princess Is In Another Castle!\n/).to_stdout
+    expect{all_phrases}.to output(/It's Dangerous To Go Alone! Take This.\n/).to_stdout
+  end
+ends
+end
